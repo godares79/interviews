d6e6413055d3a9c8a9519ba93c9d7688c42ed8d5
godares79 <david.drich@gmail.com>
2023-04-05 16:45:58 -0400

    playing around with doubly linked lists

--- python/linkedlists.py
+++ python/linkedlists.py
@@ -82,3 +82,96 @@ class SinglyLinkedList:
         while item.next:
             item = item.next
         return item
+
+
+class DoublyLinkedList:
+    """A doubly linked list."""
+
+    class ListItem:
+        def __init__(self, value=None):
+            self.value = value
+            self.next = None
+            self.prev = None
+
+        def __str__(self):
+            return "{} ({})".format(self.value, hash(self))
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __str__(self):
+        items = []
+        if not self.head:
+            return items
+
+        items.append(str(self.head))
+        next_item = self.head.next
+        while next_item:
+            items.append(str(next_item))
+            next_item = next_item.next
+
+        return " <-> ".join(items)
+
+    def __len__(self) -> int:
+        length = 0
+        item = self.head
+        while item:
+            item = item.next
+            length += 1
+        return length
+
+    def __contains__(self, item):
+        item_to_check = self.head
+        while item_to_check:
+            if item == item_to_check:
+                return True
+            item_to_check = item_to_check.next
+        return False
+
+    def add_to_head(self, value: ListItem):
+        if not self.head:
+            self.head = value
+            self.tail = value
+            return
+
+        value.next = self.head
+        self.head.prev = value
+        self.head = value
+
+    def add_to_tail(self, value: ListItem):
+        if not self.head:
+            self.head = value
+            self.tail = value
+            return
+
+        self.tail.next = value
+        value.prev = self.tail
+        self.tail = value
+
+    def remove_element(self, value: ListItem):
+        """This doesn't actually assume that the value is in the list."""
+        if not value:
+            raise Exception("The argument must not be None.")
+
+        if value == self.head and value == self.tail:
+            self.head = value.next
+            self.tail = value.prev
+            return
+        elif value == self.head:
+            value.next.prev = None
+            self.head = value.next
+            return
+        elif value == self.tail:
+            value.prev.next = None
+            self.tail = value.prev
+            return
+        else:
+            if not value.prev or not value.next:
+                # If we reach this point and the input value doesn't have a
+                # previous or next, then it is
+                raise Exception(
+                    "The input item is malformed, is it a part of this list?"
+                )
+            value.prev.next = value.next
+            value.next.prev = value.prev
--- python/tests/test_linkedlists.py
+++ python/tests/test_linkedlists.py
@@ -3,7 +3,7 @@ import unittest
 import linkedlists
 
 
-class MyTestCase(unittest.TestCase):
+class TestSinglyLinkedList(unittest.TestCase):
     def setUp(self) -> None:
         self.llist = linkedlists.SinglyLinkedList()
         self.llist.add_to_head(linkedlists.SinglyLinkedList.ListItem())
@@ -33,6 +33,63 @@ class MyTestCase(unittest.TestCase):
 
     def test_len(self):
         self.assertEqual(5, len(self.llist))
+
+
+class TestDoublyLinkedList(unittest.TestCase):
+    def setUp(self) -> None:
+        self.llist = linkedlists.DoublyLinkedList()
+        self.llist.add_to_head(linkedlists.DoublyLinkedList.ListItem())
+        self.llist.add_to_head(linkedlists.DoublyLinkedList.ListItem())
+        self.llist.add_to_head(linkedlists.DoublyLinkedList.ListItem())
+        self.middle_item = linkedlists.DoublyLinkedList.ListItem()
+        self.llist.add_to_head(self.middle_item)
+        self.llist.add_to_head(linkedlists.DoublyLinkedList.ListItem())
+        self.llist.add_to_head(linkedlists.DoublyLinkedList.ListItem())
+
+    def test_len(self):
+        self.assertEqual(0, len(linkedlists.DoublyLinkedList()))
+        self.assertEqual(6, len(self.llist))
+
+    def test_contains(self):
+        self.assertTrue(self.middle_item in self.llist)
+        other_item = linkedlists.DoublyLinkedList.ListItem()
+        self.assertFalse(other_item in self.llist)
+
+    def test_remove_element(self):
+        self.assertTrue(self.middle_item in self.llist)
+        self.llist.remove_element(self.middle_item)
+        self.assertFalse(self.middle_item in self.llist)
+
+        with self.assertRaises(Exception):
+            self.llist.remove_element(None)
+
+        with self.assertRaises(Exception):
+            # This is just some random node from somewhere, that isn't actually
+            # part of the linked list.
+            malformed_node = linkedlists.DoublyLinkedList.ListItem()
+            self.llist.remove_element(malformed_node)
+
+        # Check the case of a node being head and/or tail.
+        old_head = self.llist.head
+        starting_length = len(self.llist)
+        self.llist.remove_element(old_head)
+        self.assertEqual(self.llist.head, old_head.next)
+        self.assertFalse(old_head in self.llist)
+        self.assertEqual(starting_length - 1, len(self.llist))
+
+        old_tail = self.llist.tail
+        starting_length = len(self.llist)
+        self.llist.remove_element(old_tail)
+        self.assertEqual(self.llist.tail, old_tail.prev)
+        self.assertFalse(old_tail in self.llist)
+        self.assertEqual(starting_length - 1, len(self.llist))
+
+        tiny_list = linkedlists.DoublyLinkedList()
+        only_item = linkedlists.DoublyLinkedList.ListItem()
+        tiny_list.add_to_head(only_item)
+        tiny_list.remove_element(only_item)
+        self.assertIsNone(tiny_list.head)
+        self.assertIsNone(tiny_list.tail)
 
 
 if __name__ == "__main__":
